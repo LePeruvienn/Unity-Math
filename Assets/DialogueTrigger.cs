9fc46c3e3fc4328bbc0e5424b934df03814e529f
dory-san <doryan.jovanovic@universite-paris-saclay.fr>
2024-04-19 18:47:05 +0200

    add script

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public bool isInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
