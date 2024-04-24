a18e8b69ed8bd4952d73b1da09f62bd2c5b891f1
MiasMax <2016maxe@gmail.com>
2024-04-24 14:36:03 +0200

    Fix bug

--- Assets/Scripts/PickUpObject.cs
+++ Assets/Scripts/PickUpObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class PickUpObject : MonoBehaviour
@@ -7,14 +8,18 @@ public class PickUpObject : MonoBehaviour
     public GameObject objectToSpawn;
     public Animator animator;
     public GameObject bonusMenu;
+    private bool isOpen = false;
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
        if (collision.gameObject.CompareTag("Player"))
         {
+            if (!isOpen)
+            {
+                isOpen = true;
+                animator.SetTrigger("ChestOpening");
+                StartCoroutine("waiter_not_that_waiter_just_waiter");
 
-            animator.SetTrigger("ChestOpening");
-            StartCoroutine("waiter_not_that_waiter_just_waiter");
-
+            }
         }
     }
     IEnumerator waiter_not_that_waiter_just_waiter()
